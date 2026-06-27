f4bf70c6a5b2bfb2ee40abab685ed5a4552afac5
Patricia Baptista <patriciabaptista1808@gmail.com>
2026-06-27 17:40:36 +0100

    Tutorial » Movement: finish implementing movement + first step of tutorial that checks player has moved in all directions

--- TheCallOfNature/Assets/Scripts/Tutorial/TutorialStep_Move.cs
+++ TheCallOfNature/Assets/Scripts/Tutorial/TutorialStep_Move.cs
@@ -2,16 +2,45 @@ using UnityEngine;
 
 internal class TutorialStep_Move : AbstractTutorialStep, ITutorialStep
 {
+    // how far off from initial position counts as "moved"
+    public float threshold = 1f;
+    
+    public GameObject player;
 
-    public float threshold = 0.5f; // how far off-center counts as "moved"
-
+    private Vector3 startPosition;
     private bool movedLeft, movedRight, movedUp, movedDown;
 
     public bool TutorialMovementComplete =>
         movedLeft && movedRight && movedUp && movedDown;
 
+    void Start()
+    {
+        startPosition = player.transform.position;
+    }
+
     void Update()
     {
+        var currentPosition = player.transform.position;
+
+        // check if player has moved from initial position
+        if (Mathf.Abs(startPosition.x - currentPosition.x) > threshold
+            || Mathf.Abs(startPosition.y - currentPosition.y) > threshold)
+        {
+            if (!movedRight && currentPosition.x > startPosition.x + threshold)
+                movedRight = true;
+            else if (!movedLeft && currentPosition.x < startPosition.x - threshold)
+                movedLeft = true;
+
+            if (!movedUp && currentPosition.y > startPosition.y + threshold)
+                movedUp = true;
+            else if (!movedDown && currentPosition.y < startPosition.y - threshold)
+                movedDown = true;
+
+            if (TutorialMovementComplete)
+            {
+                this.Complete();
+            }
+        }
     }
 
 
