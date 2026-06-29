a611f8d96c39e15c3055467f54d85057002a22bd
Patricia Baptista <patriciabaptista1808@gmail.com>
2026-06-29 11:43:25 +0100

    when soudwave hits person, they run off-screen in the closest direction

--- TheCallOfNature/Assets/Scripts/PersonController.cs
+++ TheCallOfNature/Assets/Scripts/PersonController.cs
@@ -2,20 +2,37 @@ using UnityEngine;
 
 public class PersonController : MonoBehaviour
 {
+
+    [SerializeField] private float moveSpeed = 3;
+
+    private Rigidbody2D rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnParticleCollision(GameObject other)
     {
         Debug.Log($"A collider has entered the {nameof(PersonController)} trigger");
+        // find closest off-screen side and move person in that direction
+        Vector2 newPosition = gameObject.transform.position.x < 0 
+            // move left
+            ? new Vector2(gameObject.transform.position.x - Screen.width / 2f, gameObject.transform.position.y)
+            // move right
+            : new Vector2(gameObject.transform.position.x + Screen.width / 2f, gameObject.transform.position.y);
+
+        rb.linearVelocityX = newPosition.x * moveSpeed;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
