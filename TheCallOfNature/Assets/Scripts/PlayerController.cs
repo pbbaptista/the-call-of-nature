c1bca48fd9390db3cc677655eada9d33fe3d89e5
Patricia Baptista <patriciabaptista1808@gmail.com>
2026-06-29 09:16:38 +0100

    move audio detector to PlayerController; if audio loud enough, trigger soundwave

--- TheCallOfNature/Assets/Scripts/PlayerController.cs
+++ TheCallOfNature/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,12 @@ public class PlayerController : MonoBehaviour
 
     public GameObject player;
 
+    [SerializeField] private float audioThreshold = 0.1f;
+    [SerializeField] private float loudnessSensibility = 100;
+    [SerializeField] private AudioLoudnessDetection detector;
+
+    [SerializeField] private GameObject soundwave;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +19,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
+        if (loudness > audioThreshold)
+        {
+            soundwave.SetActive(true);
+            // scare people away
+            Debug.Log("Sound from mic is loud enough to scare people away");
+        }
+        else
+        {
+            soundwave.SetActive(false);
+        }
     }
 }
--- TheCallOfNature/Assets/Scripts/Tutorial/TutorialStep_ScarePeople.cs
+++ TheCallOfNature/Assets/Scripts/Tutorial/TutorialStep_ScarePeople.cs
@@ -7,9 +7,6 @@ using UnityEngine.UIElements;
 
 internal class TutorialStep_ScarePeople : AbstractTutorialStep, ITutorialStep
 {
-    [SerializeField] private float audioThreshold = 0.1f;
-    [SerializeField] private float loudnessSensibility = 100;
-    [SerializeField] private AudioLoudnessDetection detector;
     void ITutorialStep.Enter(UIDocument uiTutorialDocument, string directiveName)
     {
         Enter(uiTutorialDocument, directiveName);
@@ -32,11 +29,5 @@ internal class TutorialStep_ScarePeople : AbstractTutorialStep, ITutorialStep
 
     public void Update()
     {
-        float loudness = detector.GetLoudnessFromMicrophone() * loudnessSensibility;
-        if (loudness > audioThreshold)
-        {
-            // scare people away
-            Debug.Log("Sound from mic is loud enough to scare people away");
-        }
     }
 }
