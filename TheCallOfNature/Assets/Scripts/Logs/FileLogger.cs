545f00137510b49dd8f8edaed26e43412f0f726d
Patricia Baptista <patriciabaptista1808@gmail.com>
2026-06-30 06:57:07 +0100

    prep logfile to be shareable at end of playtest; create UI for End Screen which will show points + the logs share button

---
+++ TheCallOfNature/Assets/Scripts/Logs/FileLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public class FileLogger : MonoBehaviour
+{
+    private string logFilePath;
+    private StreamWriter writer;
+
+    void Awake()
+    {
+        string fileName = $"log_{DateTime.Now:yyyyMMdd_HHmmss}.txt";
+        logFilePath = Path.Combine(Application.persistentDataPath, fileName);
+        writer = new StreamWriter(logFilePath, true);
+        writer.AutoFlush = true;
+
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        writer.WriteLine($"[{DateTime.Now:HH:mm:ss.fff}] [{type}] {logString}");
+        if (type == LogType.Exception || type == LogType.Error)
+            writer.WriteLine(stackTrace);
+    }
+
+    public string GetLogFilePath() => logFilePath;
+
+    void OnDestroy()
+    {
+        Application.logMessageReceived -= HandleLog;
+        writer?.Flush();
+        writer?.Close();
+    }
+}
---
+++ TheCallOfNature/Assets/Scripts/Logs/LogSharer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UIElements.Button;
+
+public class LogSharer : MonoBehaviour
+{
+    [SerializeField] private FileLogger fileLogger;
+    [SerializeField] private UIDocument uiDocumentWithShareButton;
+
+    private Button shareButton;
+
+    void Start()
+    {
+        shareButton = uiDocumentWithShareButton.rootVisualElement.Q<Button>("shareButton");
+        shareButton.clicked += ShareLogFile;
+    }
+
+    void ShareLogFile()
+    {
+        // Make sure the writer's buffer is flushed before sharing
+        string path = fileLogger.GetLogFilePath();
+
+        if (!System.IO.File.Exists(path))
+        {
+            Debug.LogWarning("Log file not found at: " + path);
+            return;
+        }
+
+        new NativeShare()
+            .AddFile(path)
+            .SetSubject("Playtest Log: " + DateTime.UtcNow)
+            .SetText("Here's my session log from the playtest!")
+            .SetCallback((result, shareTarget) =>
+                Debug.Log("Share result: " + result + ", target: " + shareTarget))
+            .Share();
+    }
+
+    void OnDisable()
+    {
+        shareButton.clicked -= ShareLogFile;
+    }
+}
